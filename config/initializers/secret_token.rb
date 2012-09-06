675a0649080448bf4431febfcfe48206830a01f4
rickyuan <rushyzt@gmail.com>
2012-09-06 20:24:18 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SportsIn::Application.config.secret_token = 'c3f32e9f5551544810fb54cab5589624a5dcc1b3392c524d5b2afe2cc1ce277aacb688a2a00366db5ff95c621bb74dff8ecbd0ef2c2a0c5fc627ba301b54f3df'
